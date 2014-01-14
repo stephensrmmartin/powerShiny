51f1b1bb90393f8be20e24665e0cf7c1685dda47
Stephen Martin <stephensrmmartin@gmail.com>
2014-01-14 14:13:50 -0500

    Added ability to download plot.

--- server.R
+++ server.R
@@ -12,7 +12,7 @@ shinyServer(function(input, output) {
 		r2tof2(r2=input$r2value)
 	})
    
-  output$pwrPlot <- renderPlot({
+	output$pwrPlot <- renderPlot({
      
     # generate and plot an rnorm distribution with the requested
     # number of observations
@@ -25,6 +25,30 @@ shinyServer(function(input, output) {
   				   type=input$type,
   				   standard=input$standard)
   	print(plotPower(pwrFrame,guides=input$guides,power=input$power,df=input$dfs.plot))
-  })
+	})
+    
+	output$downloadButton <- downloadHandler(
+		filename = function(){
+			paste0("powerPlot.",as.character(input$plotFormat))
+		},
+		content = function(file){
+			pwrFrame <- createPwrFrame(effectSizes=input$effectSize,
+						   dfs=5:input$dfs,
+						   dfNumerator=input$dfNumerator,
+						   sig.level=input$sigLevel,
+						   alternative=input$alternative,
+						   test=input$test,
+						   type=input$type,
+						   standard=input$standard)
+			if(input$plotFormat == "svg"){
+				svg(file,width=input$plotWidth,height=input$plotHeight)
+			}
+			else{
+				pdf(file,width=input$plotWidth,height=input$plotHeight)
+			}
+			print(plotPower(pwrFrame,guides=input$guides,power=input$power,df=input$dfs.plot))
+			dev.off()
+		}
+		)
   
 })
